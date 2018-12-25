ad7c61b6926f8635e8766ea20c98c7dc8a58509f
Ali-X <alina240895@yandex.ru>
2018-12-25 20:37:02 +0200

    Implement fourth task

---
+++ js/script4.js
@@ -0,0 +1,22 @@
+function City(name, population, country) {
+  this.name = name;
+  this.population = population;
+  this.country = country;
+  this.getPopulation = function () {
+    return this.population;
+  };
+  this.getCityName = function () {
+    return this.country;
+  };
+  this.addCitizen = function () {
+    this.population++;
+  };
+}
+
+let kyiv = new City("Kyiv", 1000, "Ukraine");
+kyiv.addCitizen();
+kyiv.addCitizen();
+kyiv.addCitizen();
+
+console.log(kyiv.getCityName());
+console.log(kyiv.getPopulation());
